bcc6dd34b1b88a24ccd7030ed9dc0ff797e958f9
asieradzk <bluekewe94@gmail.com>
2024-06-21 20:03:58 +0200

    added cotinuous PPO

---
+++ CartPoleContAsync.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gym.Environments.Envs.Classic;
+using Gym.Rendering.WinForm;
+using OneOf;
+using RLMatrix;
+
+public class CartPoleContAsync : IContinuousEnvironmentAsync<float[]>
+{
+    public int stepCounter { get; set; }
+    public int maxSteps { get; set; }
+    public bool isDone { get; set; }
+    public OneOf<int, (int, int)> StateSize { get; set; }
+    public int[] DiscreteActionSize { get; set; }
+    (float min, float max)[] IContinuousEnvironmentAsync<float[]>.ContinuousActionBounds { get => new (float min, float max)[0];
+ set => throw new NotImplementedException(); }
+
+    private CartPoleEnv myEnv;
+    private float[] myState;
+
+    public CartPoleContAsync()
+    {
+        Task.Run(async () => await InitialiseAsync()).Wait(); // Initialize in constructor asynchronously
+    }
+
+    public Task<float[]> GetCurrentState()
+    {
+        if (isDone)
+            Reset().Wait(); // Reset if done
+
+        return Task.FromResult(myState ?? new float[4] { 0, 0, 0, 0 });
+    }
+
+    public async Task InitialiseAsync()
+    {
+        myEnv = new CartPoleEnv(WinFormEnvViewer.Factory);
+        stepCounter = 0;
+        maxSteps = 100000;
+        StateSize = myEnv.ObservationSpace.Shape.Size;
+        DiscreteActionSize = new int[] { myEnv.ActionSpace.Shape.Size };
+        await Task.Run(() => myEnv.Reset()); // Assuming Reset is not async; wrap in Task.Run
+        isDone = false;
+        myState = new float[4]; // Initialize state with default values
+    }
+
+    public Task Reset()
+    {
+        return Task.Run(() =>
+        {
+            myEnv.Reset(); // Assuming Reset is not async; wrap in Task.Run
+            myState = new float[4] { 0, 0, 0, 0 };
+            isDone = false;
+            stepCounter = 0;
+        });
+    }
+
+    Task<(float reward, bool done)> IContinuousEnvironmentAsync<float[]>.Step(int[] discreteActions, float[] continuousActions)
+    {
+        return Task.Run(() =>
+        {
+            if (isDone)
+                Reset().Wait(); // Reset if done
+
+            var actionId = discreteActions[0];
+            var (observation, reward, done, information) = myEnv.Step(actionId);
+            SixLabors.ImageSharp.Image img = myEnv.Render();
+            myState = observation.ToFloatArray();
+            isDone = done;
+
+            stepCounter++;
+            if (stepCounter > maxSteps)
+                isDone = true;
+
+            if (isDone)
+                reward = 0;
+
+            return (reward, isDone);
+        });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ var myChart = new WinformsChart();
 
 
 var optsppo = new PPOAgentOptions(
-    batchSize: 32,           // Nu8mber of EPISODES agent interacts with environment before learning from its experience
+    batchSize: 12,        // Nu8mber of EPISODES agent interacts with environment before learning from its experience
     memorySize: 10000,       // Size of the replay buffer
     gamma: 0.99f,          // Discount factor for rewards
     gaeLambda: 0.95f,      // Lambda factor for Generalized Advantage Estimation
-    lr: 1e-3f,            // Learning rate
+    lr: 1e-4f,            // Learning rate
     width: 512,
     depth: 2,
     clipEpsilon: 0.2f,     // Clipping factor for PPO's objective function
     vClipRange: 0.2f,      // Clipping range for value loss
     cValue: 0.5f,          // Coefficient for value loss
-    ppoEpochs: 20,            // Number of PPO epoch
+    ppoEpochs: 7,            // Number of PPO epoch
     clipGradNorm: 0.5f,
     entropyCoefficient: 0.005f,
     useRNN: false
@@ -46,15 +46,19 @@ var optsdqn = new DQNAgentOptions(numAtoms: 51,
 
 
 
-var env = new List<IEnvironmentAsync<float[]>> { new CartPoleAsync(), new CartPoleAsync(), new CartPoleAsync(), new CartPoleAsync(), new CartPoleAsync(), new CartPoleAsync(), };
+var env = new List<IContinuousEnvironmentAsync<float[]>> { new TrivialContinuousEnvironmentAsync(), };
 //----------------------------------can use PPO options \/ or DQN options
-var myAgent = new LocalDiscreteRolloutAgent<float[]>(optsdqn, env, myChart);
+var myAgent = new LocalContinuousRolloutAgent<float[]>(optsppo, env, myChart);
 
-
+/*
+var env = new List<IEnvironmentAsync<float[]>> { new CartPoleAsync() };
+//----------------------------------can use PPO options \/ or DQN options
+var myAgent = new LocalDiscreteRolloutAgent<float[]>(optsppo, env, myChart);
+*/
 
 //var savePath = @"C:\temp";
 //await myAgent.Load(savePath);
-for (int i = 0; i < 46000; i++)
+for (int i = 0; i < 500; i++)
 {
    await myAgent.Step();
 }
@@ -62,9 +66,9 @@ for (int i = 0; i < 46000; i++)
 
 Console.WriteLine("doneTraining");
 
-for (int i = 0; i < 8000; i++)
+for (int i = 0; i < 800; i++)
 {
-    await myAgent.Step(false);
+    await myAgent.Step(true);
 
 }
 
